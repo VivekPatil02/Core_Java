e882583b78f810b64535679ee8083449b7be1961
VivekPatil02 <vivekpatil5627@gmail.com>
2024-02-01 06:50:21 +0530

    created other class for addition operation

--- Inheritance.java
+++ Inheritance.java
@@ -6,9 +6,20 @@ class Animal {
     }
 }
 
+class Calculator{
+    public int add(int n1,int n2){
+        int result = n1+n2;
+        return result;
+    }
+}
+
 public class Inheritance {
     public static void main(String[] args) {
         Animal dog = new Animal();
         dog.eat();
+
+        Calculator calc = new Calculator();
+        int addition = calc.add(5,6);
+        System.out.println(addition);
     }
 }
