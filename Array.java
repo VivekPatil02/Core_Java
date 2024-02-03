d9b2303c798d964f4305a168865ccf03b636d3e1
VivekPatil02 <vivekpatil5627@gmail.com>
2024-02-03 08:05:40 +0530

    Array notes

--- Array.java
+++ Array.java
@@ -1,9 +1,9 @@
+package Core_Java;
 public class Array {
     public static void main(String[] args) {
         // we use array to assign multiple values to one variable
-        // here num which refers to a variable, we have to use [] to indicate this is an
-        // array var and unlike in javascript or any other ptogramming language we can't
-        // assign values inside square braces [] but in curly braces {}
+        // here num which refers to a variable, we have to use [] to indicate this is an array var 
+        // and unlike in javascript or any other programming language we can't assign values inside square braces [] but in curly braces {}
         int num[] = { 4, 5, 8, 9 };
         System.out.println(num[2]); // ans is 8 because indexing starts from 0th index.
 
