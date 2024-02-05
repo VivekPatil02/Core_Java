3178ae7797ca5a64747d8f68f1a663380b9f0a2b
VivekPatil02 <vivekpatil5627@gmail.com>
2024-02-05 16:17:15 +0530

    array declaration at run time

--- Loop.java
+++ Loop.java
@@ -2,7 +2,6 @@ package Core_Java;
 public class Loop {
     public static void main(String[] args) {
         //while loop
-
         //in while loop unlike for loop, only condition required as param...behavior increment i++ defined in loop block and initialization outside the block.
         
         int i = 0;
@@ -11,8 +10,7 @@ public class Loop {
             i++;
         }
 
-        //for loop
-        
+        //for loop   
         //in for loop three params which are 1.initialization, 2. condition and 3.behavior(increment/decrement).here in for loop j=0 is initialization, j<=10 is condition and j++ is increment behavior
         for (int j=0; j<=10 ; j++){
             System.out.println("for loop printing here " + j);
