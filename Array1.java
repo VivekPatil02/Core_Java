a9d8791baeaeaccdc646cc94236f65b6341a5a27
VivekPatil02 <vivekpatil5627@gmail.com>
2024-02-09 06:34:07 +0530

    complex array problem

---
+++ Array1.java
@@ -0,0 +1,31 @@
+class Human {
+    String Name;
+    int Marks;
+}
+
+public class Array1 {
+    public static void main(String[] args) {
+
+        Human obj1 = new Human();
+        obj1.Name = "Vivek";
+        obj1.Marks = 98;
+
+        Human obj2 = new Human();
+        obj2.Name = "Rahul";
+        obj2.Marks = 68;
+
+        Human obj3 = new Human();
+        obj3.Name = "sunil";
+        obj3.Marks = 47;
+
+        Human num[] = new Human[3];
+        num[0] = obj1;
+        num[1] = obj3;
+        num[2] = obj2;
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println(num[i].Name + " : " + num[i].Marks);
+        }
+
+    }
+}
--- MultiDimentionalArray.java
+++ MultiDimentionalArray.java
@@ -2,6 +2,18 @@ package Core_Java;
 
 public class MultiDimentionalArray {
     public static void main(String[] args) {
-        
+
+        int num[][] = new int[3][4];
+        // num[0] = new int[4];
+        // num[1] = new int[3];
+        // num[2] = new int[2];
+
+        for (int i = 0; i < num.length; i++) {
+            for (int j = 0; j < num[i].length; j++) {
+                // num[i][j] = (int)(Math.random() * 10);
+                System.out.print(num[i][j]);
+            }
+            System.out.println();
+        }
     }
 }
