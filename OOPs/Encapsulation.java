c6aaab7e3925135398c947d1a3bc64b6c88b135c
VivekPatil02 <vivekpatil5627@gmail.com>
2024-02-11 23:27:03 +0530

    accessing private properties through getter methods

--- OOPs/Encapsulation.java
+++ OOPs/Encapsulation.java
@@ -1,5 +1,20 @@
 package OOPs;
-
+class Employee{
+    private String name= "vivek";
+    private int age=10;
+    public int getAge(){
+        return age;
+    }
+    public String getName(){
+        return name;
+    }
+}
+//what is encapsulation?
 public class Encapsulation {
     
+    public static void main(String[] args) {
+        Employee e1 = new Employee();
+        System.out.println(e1.getAge());
+        System.out.println(e1.getName());
+    }
 }
